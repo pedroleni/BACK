e648e7d9fcc54f515a4a2b17d3eedb14a022920f
Pedro Lérida Nieto <108528939+pedroleni@users.noreply.github.com>
2022-09-15 01:54:07 +0200

    Update plataforma.model.js
    update model to plataforma

--- src/api/plataforma/plataforma.model.js
+++ src/api/plataforma/plataforma.model.js
@@ -6,15 +6,7 @@ const schema = new Schema(
 		name: { type: String },
 		company: { type: String },
 		year: { type: String },
-		image: { type: String },
-
-		// juego:{type: String },
-		// plataforma:{type: String },
-		// comentarios:{type: String },
-		// likes: {type: Array, default: []},
-		// juego: [{ type: Schema.Types.ObjectId, ref: "companies", required: true }],
-		// plataformas: [{ type: Schema.Types.ObjectId, ref: "companies", required: true }],
-		// comentarios: [{ type: Schema.Types.ObjectId, ref: "companies", required: true }],
+		image: { type: String }
 	},
 	{
 		timestamps: true,
